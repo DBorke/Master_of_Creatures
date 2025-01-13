7d880d45cd88ed912528626e15dc5d166f515f3d
DBorke <dannybborke@gmail.com>
2025-01-13 08:29:11 +0100

    Fundamental set up of Maven.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,24 @@
-module dtu.master_of_creatures {
+module dtu.master_of_creatures
+{
+    // JavaFX libraries
+    requires javafx.base;
     requires javafx.controls;
+    requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.swing;
+    requires javafx.media;
 
+    // Maven libraries
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires common;
 
+    // Class allowances
     opens dtu.master_of_creatures to javafx.fxml;
     exports dtu.master_of_creatures;
 }
