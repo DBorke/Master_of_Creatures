ac2ed13c33a16e2fe599bac881d936e0cf806bdf
DBorke <dannybborke@gmail.com>
2025-01-13 08:23:18 +0100

    Initial commit.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module dtu.master_of_creatures {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens dtu.master_of_creatures to javafx.fxml;
+    exports dtu.master_of_creatures;
+}
